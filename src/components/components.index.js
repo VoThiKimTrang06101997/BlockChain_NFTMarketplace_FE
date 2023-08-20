275ca6d8fa2026c2fa50283f6a2c051399f4e879
VoThiKimTrang06101997 <vothikimtrang0610@gmail.com>
2023-08-21 00:09:52 +0700

    Implement: Service and BigNFTSlider

--- src/components/components.index.js
+++ src/components/components.index.js
@@ -2,5 +2,7 @@ import Button from "./Button/Button";
 import Footer from "./Footer/Footer";
 import NavBar from "./Navbar/Navbar";
 import HeroSection from "./HeroSection/HeroSection";
+import Service from "./Service/Service";
+import BigNFTSlider from "./BigNFTSlider/BigNFTSlider";
 
-export { Button, NavBar, Footer, HeroSection };
+export { Button, NavBar, Footer, HeroSection, Service, BigNFTSlider };
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,20 @@ import React from "react";
 
 //INTERNAL IMPORT
 import Style from "../styles/index.module.css";
-import { HeroSection} from "../components/components.index";
+import {
+  BigNFTSlider,
+  HeroSection,
+  Service,
+} from "../components/components.index";
 
 const Home = () => {
   return (
     <div className={Style.homePage}>
       <HeroSection />
+      <Service />
+      <BigNFTSlider />
     </div>
   );
 };
 
 export default Home;
-
